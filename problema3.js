6fda8872868768a98b07aa15c6e27a0ee1b3e4f4
DIΣGӨ L <di3golabrin@gmail.com>
2024-04-30 09:42:16 -0400

    problema 2

---
+++ problema3.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3055;
+
+// Configuración del motor de plantillas EJS
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.render("in_persona_02");
+});
+
+app.post("/procesar", (req, res) => {
+    const { nombre, apellido, direccion, fechaNacimiento, comentario} = req.body;
+
+    // Calcula la edad a partir de la fecha de nacimiento 
+    const fechaNacimientoDate = new Date(fechaNacimiento);
+    const ahora = new Date();
+    const edad = ahora.getFullYear() - fechaNacimientoDate.getFullYear();
+    // contar cuantos carácteres numericos tiene el comentario
+    const cantidadNumerosComentario = comentario.replace(/\D/g, "").length;
+
+    // Renderiza la vista de resultado
+    res.render("out_persona_02", {
+    nombre,
+    apellido,
+    direccion,
+    edad,
+    cantidadNumerosComentario
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor web escuchando en el puerto ${port}`);
+});
