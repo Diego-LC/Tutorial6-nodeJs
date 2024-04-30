bca2cf6e0443382d36d472e27425226a49a4937f
DIΣGӨ L <di3golabrin@gmail.com>
2024-04-30 08:52:40 -0400

    problema 1

--- problema1.js
+++ problema1.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000; // Puerto en el que se ejecutará el servidor
+const port = 3055; // Puerto en el que se ejecutará el servidor
+
 // Ruta que ejecutará el programa "Hola Mundo"
 app.get("/", (req, res) => {
-    res.send("Hola Mundo desde la web");
+    res.send("Diego Labrin desde la web");
 });
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log("Servidor web escuchando en el puerto ${port}");
+    console.log(`Servidor web escuchando en el puerto ${port}`);
 });
